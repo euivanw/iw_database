684d3c2a43edf6ed8cb13b30d234adf04bcc7564
Ivan Wilhelm <ivan.wilhelm@bv.com.br>
2023-11-27 18:52:47 -0300

    Provided classes documentation

--- lib/src/services/database_client_service.dart
+++ lib/src/services/database_client_service.dart
@@ -1,10 +1,15 @@
 import '../models/database_result_model.dart';
 
+/// This interface represents the service responsible to implemented the
+/// required methods to implement a database client.
 abstract interface class DatabaseClientService {
+  /// Responsible to open a database connection.
   Future<void> openConnection();
 
+  /// Responsible to disconnected from a database already connected.
   Future<void> closeConnection();
 
+  /// Responsible to execute a SQL query into the database.
   Future<DatabaseResult> executeQuery({
     required String sql,
     Map<String, dynamic>? parameters,
